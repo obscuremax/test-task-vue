cdc7a54a08a03b1c81123a6b741025e5fbaa75f8
obscuremax <53945011+obscuremax@users.noreply.github.com>
2022-02-21 04:43:42 +0300

    Update api.js

--- github-graphql/src/API/api.js
+++ github-graphql/src/API/api.js
@@ -1,7 +1,7 @@
 export default function config(conf) {
   
   const url = "https://api.github.com/graphql";
-  const token = "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";
+  const token = "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";
   
   const headers = { 
     'Authorization': `Bearer ${token}`, 
